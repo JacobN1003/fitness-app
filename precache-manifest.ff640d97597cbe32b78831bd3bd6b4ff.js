0ab1f33e49fd43d9516ca07ae05a254c2e9a39e4
Jacob Norenberg <jacobn@Jacob.attlocal.net>
2020-08-11 18:24:40 -0700

    Updates

---
+++ precache-manifest.ff640d97597cbe32b78831bd3bd6b4ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2be33f3dd659f2850507e41385a17f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aed0f476115f286a477e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7dfb914ab0af88da98aa",
+    "url": "/static/js/2.06fd625a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.06fd625a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aed0f476115f286a477e",
+    "url": "/static/js/main.7284a3c3.chunk.js"
+  },
+  {
+    "revision": "15d112e68016dcc27418",
+    "url": "/static/js/runtime-main.aa5fb3f4.js"
+  }
+]);
