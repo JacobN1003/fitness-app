9f96924c9ff7c89e74759b542602146cf3378fb1
Jacob Norenberg <jacobn@jacob.lan>
2020-12-13 14:26:59 -0800

    v1.2

--- Server.js
+++ Server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-const { PORT } = process.env 
+const { PORT } = process.env || 5001
 
 app.use(cookieParser())
 app.use(bodyParser.json())
